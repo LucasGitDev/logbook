743aa7fbedbed921d397b28584a64abf4f7e7819
Lucas Teles <lucas.teles@scoder.com.br>
2026-05-28 15:27:45 -0300

    feat(stores): add zustand vault store stub

---
+++ src/stores/vaultStore.ts
@@ -0,0 +1,21 @@
+import { create } from 'zustand'
+import type { VaultState } from '../types/vault'
+
+interface VaultStore extends VaultState {
+  setRootHandle: (handle: FileSystemDirectoryHandle | null) => void
+  reset: () => void
+}
+
+const initialState: VaultState = {
+  rootHandle: null,
+  tasks: [],
+  agendaItems: [],
+  projects: [],
+  isLoaded: false,
+}
+
+export const useVaultStore = create<VaultStore>((set) => ({
+  ...initialState,
+  setRootHandle: (rootHandle) => set({ rootHandle, isLoaded: rootHandle !== null }),
+  reset: () => set(initialState),
+}))
